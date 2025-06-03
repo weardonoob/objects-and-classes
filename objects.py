cc6538aa0332e978c27ba575c6d459ace868815c
weardonoob <sharma.alexander@icloud.com>
2025-06-03 17:56:27 +0200

    Add files via upload

--- objects.py
+++ objects.py
@@ -6,25 +6,34 @@ pygame.display.set_caption("objects and classes")
 screen.fill("white")
 
 pygame.display.update()
-
-class Rectangle():
-    def __init__(self, c, x ,y ,w,h):
-        self.c = c
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        print("hello")
+class Circle():
+    def __init__(self,c,cx,cy,ra):
+         self.c = c
+         self.cy = cy
+         self.cx = cx
+         self.ra = ra
+         print("hi")
     def draw(self):
-        pygame.draw.rect(screen, self.c,(self.x,self.y,self.w, self.h))
+         pygame.draw.circle(screen, self.c, (self.cx,self.cy) , self.ra)
+# class Rectangle():
+#     def __init__(self, c, x ,y ,w,h):
+#         self.c = c
+#         self.x = x
+#         self.y = y
+#         self.w = w
+#         self.h = h
+#         print("hello")
+#     def draw(self):
+#         pygame.draw.rect(screen, self.c,(self.x,self.y,self.w, self.h))
 
+blue = Circle("blue", 100,100,100)
+blue.draw()
+# red = Rectangle("red", 100,200,100,50)
+# red.draw()
 
-red = Rectangle("red", 100,200,100,50)
-red.draw()
+# green = Rectangle("green", 300,200,100,150)
+# green.draw()
 
-green = Rectangle("green", 300,200,100,150)
-green.draw()
-
-yellow = Rectangle("yellow", 300,200,150,50)
-yellow.draw()
+# yellow = Rectangle("yellow", 300,200,150,50)
+# yellow.draw()
 pygame.display.update()
